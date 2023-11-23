d643473c32e954957f04d1c3dbeb3fd71147e05a
Moin-Islam <pkarnival@gmail.com>
2023-11-23 17:39:31 +0600

    updated chat apis

--- app/chat.js
+++ app/chat.js
@@ -5,15 +5,35 @@ app.controller("ChatWindowCtrl", [
   function ($scope) {
     const userId = sessionStorage.getItem("userId");
     const receiverInfo = JSON.parse(sessionStorage.getItem("receiverInfo"));
-    console.log(receiverInfo);
+    console.log(userId);
+    //console.log(sessionStorage);
 
     const { id, name, image,status } = receiverInfo;
-    console.log(id,name,image);
+    console.log(id);
 
     $scope.receiverName = name;
     $scope.receiverImage = image;
     $scope.receiverStatus = status;
 
+    $scope.FetchUserMessage = function (){
+
+      console.log("aa");
+      $.ajax({
+        type: "POST",
+        url: "http://localhost/api/services/MessageServices.php?service=FetchUserMessage",
+        data: JSON.stringify({
+          "sender_id" : userId,
+          "receiver_id" : id
+        }),
+        success: function (response) {
+          console.log(response.message);
+          $scope.$applyAsync();
+          $scope.UserMessages = response.message;
+        }
+      });
+    }
+    $scope.FetchUserMessage();
+    console.log($scope.UserMessages);
     
     
   },
